c42d6b75b9b664e8d8db01c703ccbd89fd1338c3
Rian de Oliveira <91153204+RianOliveiraL@users.noreply.github.com>
2022-09-28 21:22:11 -0300

    Add files via upload

---
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -0,0 +1,45 @@
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+import {SubmittedItem} from "../../model/submitted-item";
+
+@Component({
+  selector: 'app-todo-input-add-items',
+  templateUrl: './todo-input-add-items.component.html',
+  styleUrls: ['./todo-input-add-items.component.scss']
+})
+export class TodoInputAddItemsComponent implements OnInit, OnChanges {
+
+  public buttonName: string = 'Add';
+  @Output() public emitItem = new EventEmitter();
+  @Input() public task: string = '';
+  @Input() public index?: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    if(this.index != undefined)
+      this.buttonName = 'update';
+  }
+
+  public submitItem() {
+    if (this.task.trim()) {
+      const submittedItem: SubmittedItem = {
+        task: this.task,
+        index: this.index
+      };
+
+      this.emitItem.emit(submittedItem);
+    }
+
+    this.reset();
+  }
+
+  public reset() {
+    this.task = '';
+    this.index = undefined;
+    this.buttonName = 'Add';
+    this.emitItem.emit(undefined);
+  }
+}
